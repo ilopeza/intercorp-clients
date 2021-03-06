788cb603870e799b211b821e2a223e0460e7a75a
ilopeza <ilopezarzuaga@gmail.com>
2021-03-06 12:01:36 -0300

    chore: add DDl for h2 db. Insert test data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+-- PUBLIC.CLIENTS definition
+
+-- Drop table
+DROP TABLE clients;
+CREATE TABLE clients (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  first_name VARCHAR(250) NOT NULL,
+  last_name VARCHAR(250) NOT NULL,
+  birthdate DATE DEFAULT NULL
+);
+
+INSERT INTO clients (first_name, last_name, birthdate) VALUES
+('Aliko', 'Dangote', CURRENT_TIMESTAMP()),
+('Bill', 'Gates', CURRENT_TIMESTAMP()),
+('Folrunsho', 'Alakija', CURRENT_TIMESTAMP());
