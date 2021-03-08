a734056a34acea96fc5b904c64cf85753196bc45
ilopeza <ilopezarzuaga@gmail.com>
2021-03-08 13:32:45 -0300

    fix: swagger failed

--- src/main/java/com/intercorp/exercise/clients/config/SwaggerConfig.java
+++ src/main/java/com/intercorp/exercise/clients/config/SwaggerConfig.java
@@ -2,11 +2,16 @@ package com.intercorp.exercise.clients.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
 
 @Configuration
 @EnableSwagger2
@@ -16,8 +21,23 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.intercorp.exercise.clients.controllers;"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .pathMapping("")
+                .apiInfo(getApiInformation())
+                .directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo getApiInformation() {
+        return new ApiInfo("Intercorp-Clients",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "");
     }
 }
